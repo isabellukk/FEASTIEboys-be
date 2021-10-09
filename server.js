0613bfcb7b1dd0e563a6caefe90b06c39d624b76
Isabellukk <isabellukk@gmail.com>
2021-10-08 22:57:13 -0700

    fixed bug in server

--- server.js
+++ server.js
@@ -11,14 +11,15 @@ const recipebook = require('./controllers/recipebook.js')
 const whitelist = ["http://localhost:3000"];
 
 const corsOptions = {
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) != -1 || !origin) {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   }
 };
+
 
 
 // Middleware
